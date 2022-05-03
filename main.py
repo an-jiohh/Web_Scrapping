2ee01ad52dc6a7b647345bad1b50ff5ca56b2d25
An jiho <dkswlgh99@naver.com>
2022-05-03 19:38:23 +0900

    get_pagenumber

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+indeed_resul =  requests.get('https://www.indeed.com/jobs?q=python&limit=50')
+
+indeed_soup = BeautifulSoup(indeed_resul.text, "html.parser")
+
+pagination = indeed_soup.find("div",{"class":"pagination"})
+
+pages = pagination.find_all('a')
+
+spans = []
+for page in pages :
+    spans.append(page.find("span"))
+
+spans = spans[0:-1]
+
+print(spans)
